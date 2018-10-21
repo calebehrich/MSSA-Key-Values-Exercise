7189f2881e3c669f19d84f60ddfaee2d1013ed15
caleb <cehrich02@gmail.com>
2018-10-21 14:28:31 -0400

    Commit Init

---
+++ KeyValues/KeyValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValues
+{
+    struct KeyValue
+    {
+        public string key { get; }
+        public Object value { get; }
+
+        public KeyValue(string key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ KeyValues/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValues
+{
+    class MyDictionary
+    {
+
+        public KeyValue[] keyValueArr = new KeyValue[50];
+        int counter = -1;
+
+        public Object this[string key]
+        {
+            get
+            {
+                for (int i = 0; i < keyValueArr.Length; i++)
+                {
+                    if(key ==keyValueArr[i].key)
+                    {
+                        return keyValueArr[i].value;
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                bool replace = false;
+                for(int i = 0; i < counter; i++)
+                {
+                    if(key == keyValueArr[i].key)
+                    {
+                        keyValueArr[i] = new KeyValue(key, keyValueArr[i].value);
+                        replace = true;
+                    }
+                }
+                if(replace == false)
+                {
+                    keyValueArr[counter + 1] = new KeyValue(key, value);
+                }
+                counter++;
+            }
+        }
+    }
+}
